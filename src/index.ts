5e8de4d580af305f30df1f6de715fab015f1dd3d
webees <webees@qq.com>
2019-05-05 20:21:50 +0800

    -

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,31 @@ import Bech32 from 'bech32'
 import Blake from 'blakejs'
 import BlakeHash from 'blake-hash'
 
-
 const base58 = BaseX('123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz')
 
-const _has0x = (str: string) => str.slice(0, 2).toLowerCase() === '0x'
-const _add0x = (str: string) => _has0x(str) ? str : '0x' + str
-const _del0x = (str: string) => _has0x(str)  ? str.slice(2) : str
-
-const sha256 = (msg: any): string => {
-  if (_has0x(msg)) msg = CryptoJS.enc.Hex.parse(_del0x(msg))
-  return _add0x(CryptoJS.SHA256(msg).toString(CryptoJS.enc.Hex))
+const sha256 = (msg: string, type?: string): string => {
+  if (type === 'hex') msg = CryptoJS.enc.Hex.parse(msg)
+  return CryptoJS.SHA256(msg).toString(CryptoJS.enc.Hex)
 }
 
-const blake256 = (msg: any): string => {
-  if (_has0x(msg)) msg = Buffer.from(_del0x(msg), 'hex')
-  return _add0x(BlakeHash('blake256').update(msg).digest('hex'))
+const blake256 = (msg: any, type?: string): string => {
+  if (type === 'hex') msg = Buffer.from(msg, 'hex')
+  return BlakeHash('blake256').update(msg).digest('hex')
 }
 
-const keccak256 = (msg: any): string => {
-  if (_has0x(msg)) msg = CryptoJS.enc.Hex.parse(_del0x(msg))
-  return _add0x(CryptoJS.SHA3(msg, { outputLength: 256 }).toString(CryptoJS.enc.Hex))
+const keccak256 = (msg: any, type?: string): string => {
+  if (type === 'hex') msg = CryptoJS.enc.Hex.parse(msg)
+  return CryptoJS.SHA3(msg, { outputLength: 256 }).toString(CryptoJS.enc.Hex)
 }
 
-const blake2b = (msg: any, len: number): string => {
-  if (_has0x(msg)) msg = Buffer.from(_del0x(msg), 'hex')
-  return _add0x(Blake.blake2bHex(msg, null, len))
+const blake2b = (msg: any, len: number, type?: string): string => {
+  if (type === 'hex') msg = Buffer.from(msg, 'hex')
+  return Blake.blake2bHex(msg, null, len)
 }
 
 const base58Decode = (str: string) => {
   try {
-    return _add0x(base58.decode(str).toString('hex'))
+    return base58.decode(str).toString('hex')
   } catch (e) {
     return null
   }
@@ -50,17 +45,17 @@ const bech32Decode = (str: string) => {
 const checksum = (hex: string, hash: string) => {
   switch (hash) {
     case 'sha256':
-      return sha256(sha256(hex)).substr(0, 10)
+      return sha256(sha256(hex, 'hex'), 'hex').substr(0, 8)
     case 'blake256':
-      return blake256(blake256(hex)).substr(0, 10)
+      return blake256(blake256(hex, 'hex'), 'hex').substr(0, 8)
     case 'keccak256':
-      return keccak256(hex).substr(0, 10)
+      return keccak256(hex, 'hex').substr(0, 8)
     default:
       return null
   }
 }
 
-export default {
+const CryptoHelper = {
   sha256,
   blake256,
   keccak256,
@@ -69,3 +64,5 @@ export default {
   blake2b,
   checksum
 }
+
+export default CryptoHelper
